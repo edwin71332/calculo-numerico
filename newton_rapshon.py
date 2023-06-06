578d64a26cefea689bde888980a2cb0f0d4ee8ea
edwin71332 <edwin71332@gmail.com>
2023-06-06 15:49:33 -0400

    Initial commit

---
+++ newton_rapshon.py
@@ -0,0 +1,33 @@
+from sympy import *
+from math import e
+
+x = Symbol('x')
+f = sin(x) - e**(-x)
+
+def newton_raphson(f, x0, tol, max_iter):
+    
+    df = diff(f, x)
+    f = lambdify(x, f) 
+    df = lambdify(x, df) 
+    
+    xi = x0  
+    iteraciones = 0
+    raiz = 0
+    error = 100
+    
+    while (iteraciones < max_iter and error > tol ):
+        xi1 = xi - f(xi)/df(xi)
+        error = abs((xi1 - xi)/xi1)
+        if error < tol:          
+            raiz = xi1      
+        xi = xi1          
+        iteraciones += 1
+    print(f"La raíz encontrada es:",raiz)
+    print("el error es:",error)
+
+x0 = float(input("Ingrese la aproximación inicial xi:"))      
+tol = float(input("Ingrese la tolerancia deseada: "))        
+max_iter = int(input("Ingrese el número máximo de iteraciones permitidas: "))
+
+newton_raphson(f, x0, tol, max_iter)
+   
